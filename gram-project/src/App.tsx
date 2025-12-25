cb6592b2b8314d5ff920896634c0f52f5c19c4b1
postwo <codo771@naver.com>
2025-12-25 16:42:11 +0900

    redux 초기화 처리

--- gram-project/src/App.tsx
+++ gram-project/src/App.tsx
@@ -5,8 +5,21 @@ import './css/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignupPage from './pages/SingupPage';
 import AdminPage from './pages/AdminaPage';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { setUserFromToken } from './store/UserSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = Cookies.get('accessToken');
+    if (token) {
+      dispatch(setUserFromToken(token)); // 토큰 디코딩해서 Redux에 유저 정보 저장
+    }
+  }, [dispatch]);
+
   return (
     // 각 페이지 적용
     <Router>
